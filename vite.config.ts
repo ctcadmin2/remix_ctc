faf71320d7abe242c897cc05384f75499e1ee939
Sergiu Cozma <lssjbrolli@gmail.com>
2025-11-23 17:02:04 +0200

    bump to latest remix

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-installGlobals();
+declare module "@remix-run/node" {
+  // or cloudflare, deno, etc.
+  interface Future {
+    v3_singleFetch: true;
+  }
+}
 
 export default defineConfig({
   build: {
-    target: "es2022",
+    target: "es2024",
   },
   server: {
     port: 3000,
@@ -15,7 +19,15 @@ export default defineConfig({
   plugins: [
     remix({
       ignoredRouteFiles: ["**/.*"],
-      future: { v3_fetcherPersist: true },
+      future: {
+        v3_fetcherPersist: true,
+        unstable_optimizeDeps: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_lazyRouteDiscovery: true,
+        v3_singleFetch: true,
+        v3_routeConfig: true,
+      },
     }),
     tsconfigPaths(),
   ],
