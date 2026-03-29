62c3836a5ec2d252e910d40968db00384151e18a
Sergiu Cozma <lssjbrolli@gmail.com>
2026-03-29 17:39:53 +0300

    prisma 7 tweaks

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,25 +1,15 @@
 import { PrismaClient } from "prisma/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-
-let db: PrismaClient;
-
-declare global {
-  var __db: PrismaClient | undefined;
-}
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL!,
 });
-
-// this is needed because in development we don't want to restart
-// the server with every change, but we want to make sure we don't
-// create a new connection to the DB with every change either.
-if (process.env.NODE_ENV === "production") {
-  db = new PrismaClient({ adapter });
-} else {
-  if (!global.__db) {
-    global.__db = new PrismaClient({ adapter });
-  }
-  db = global.__db;
-}
-
-export { db };
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+const db =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+export default db;
